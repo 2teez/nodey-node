d3cee0ac775f142f9869873e37d09c8a96b21fdf
timothy adigun <adigun.timothy@gmail.com>
2026-06-15 19:41:14 +0100

    added all the functions needed

--- learning-node/ch02/notes/notes.js
+++ learning-node/ch02/notes/notes.js
@@ -1,4 +1,17 @@
 "use strict";
 
-console.log("Starts Here!");
+export const addNote = (title, body) => {
+  console.log("Adding note", title, body);
+};
 
+export const getAll = () => {
+  console.log("Getting all notes");
+};
+
+export const getNote = (title) => {
+  console.log("Getting note", title);
+};
+
+export const removeNote = (title) => {
+  console.log("Removing note", title);
+};
