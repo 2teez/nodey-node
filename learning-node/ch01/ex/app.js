3dcfa027c07a16ccff6420a61d001ad6e8f3d248
timothy adigun <adigun.timothy@gmail.com>
2026-06-15 10:56:07 +0100

    using commonjs type

---
+++ learning-node/ch01/ex/app.js
@@ -0,0 +1,6 @@
+"use strict";
+
+//import {} from "./notes.js";
+const notes = require("./notes.cjs");
+
+console.log(notes);
---
+++ learning-node/ch01/ex/notes.cjs
@@ -0,0 +1,5 @@
+"use strict";
+
+console.log("Starting notes.js");
+
+console.log(module); // use only in CommonJS
