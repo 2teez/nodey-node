0c14fc11c7124750349ec41054d596579ac86e3a
timothy adigun <adigun.timothy@gmail.com>
2026-06-16 15:52:41 +0100

    understanding basics of async in js

--- playground/async-basics.js
+++ playground/async-basics.js
@@ -2,3 +2,12 @@
 
 console.log("Starts Here!");
 
+setTimeout(() => {
+  console.log("Inside callback..");
+}, 2000);
+
+setTimeout(() => {
+  console.log("Second setTimeout");
+}, 0);
+
+console.log("Finishing up...");
