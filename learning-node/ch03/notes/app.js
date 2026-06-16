919ab0855eb732ed5de63e5d1f86b4d258d62be1
timothy adigun <adigun.timothy@gmail.com>
2026-06-16 02:46:22 +0100

    continuation with saveNotes functions

---
+++ learning-node/ch03/notes/app.js
@@ -0,0 +1,21 @@
+"use strict";
+
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+import { addNote, getAll, removeNote } from "./notes.js";
+
+const argv = yargs(hideBin(process.argv)).argv;
+const command = argv._[0];
+
+if (process.argv.length < 3) {
+  console.log("Please provide a command.");
+  process.exit(1);
+}
+
+if (command === "add") {
+  addNote(argv.title, argv.body);
+} else if (command === "list") {
+  getAll();
+} else if (command === "remove") {
+  removeNote(argv.title);
+}
---
+++ learning-node/ch03/notes/notes.js
@@ -0,0 +1,62 @@
+"use strict";
+
+import { readFileSync, writeFileSync } from "node:fs";
+
+export const addNote = (title, body) => {
+  const notes = fetchNotes();
+  const note = {
+    title,
+    body,
+  };
+
+  if (!title?.trim() || !body?.trim()) {
+    console.log("Title and body must not be empty");
+  }
+
+  if (notes.length === 0) {
+    notes.push(note);
+    saveNotes(notes);
+  } else if (removeNotesDuplicates(notes, note.title)) {
+    notes.push(note);
+    saveNotes(notes);
+  } else {
+    console.log("Note already exists");
+  }
+};
+
+export const getAll = () => {
+  fetchNotes().forEach((note) =>
+    console.log(`\nTitle: ${note.title}\nBody: ${note.body}\n`),
+  );
+};
+
+export const removeNote = (title) => {
+  const notes = fetchNotes();
+  if (notes.length === 0) {
+    console.log("Empty notes list");
+    return;
+  }
+  const filteredNotes = notes.filter((note) => note.title !== title);
+  if (notes.length !== filteredNotes.length) {
+    saveNotes(filteredNotes);
+  } else {
+    console.log("Note not found");
+  }
+};
+
+const fetchNotes = () => {
+  try {
+    const data = readFileSync("note-data.json");
+    return JSON.parse(data);
+  } catch (error) {
+    return [];
+  }
+};
+
+const removeNotesDuplicates = (notes, title) => {
+  return notes.find((note) => note.title === title) === undefined;
+};
+
+const saveNotes = (notes) => {
+  writeFileSync("note-data.json", JSON.stringify(notes, null, 2));
+};
