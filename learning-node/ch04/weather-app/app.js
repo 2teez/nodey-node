f7ab1e4df382ca53079f866883d591f3316293a2
timothy adigun <adigun.timothy@gmail.com>
2026-06-16 18:25:00 +0100

    using fetcher api in nodejs

--- learning-node/ch04/weather-app/app.js
+++ learning-node/ch04/weather-app/app.js
@@ -1,4 +1,22 @@
 "use strict";
 
-console.log("Starts Here!");
+const API_KEY = "ab45724cc15822eeff6cc675a219acb4";
 
+const url = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${API_KEY}&units=metric`;
+
+const fetchWeather = async () => {
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`HTTP Error: ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error("Error fetching weather:", error.message);
+  }
+};
+
+fetchWeather();
