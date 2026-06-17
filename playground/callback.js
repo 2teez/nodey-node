f031906d9bd375120c1469c01028a15571827886
timothy adigun <adigun.timothy@gmail.com>
2026-06-17 20:27:42 +0100

    using js callback

---
+++ playground/callback.js
@@ -0,0 +1,57 @@
+"use strict";
+// using callback
+const judge = (grade) => {
+  switch (grade) {
+    case "A":
+      return "Excellent";
+      break;
+    case "B":
+      return "Good";
+      break;
+    case "C":
+      return "Average";
+      break;
+    case "D":
+      return "Poor";
+      break;
+    case "F":
+      return "Fail";
+      break;
+    default:
+      return "Invalid grade";
+      break;
+  }
+};
+
+const getGrade = (score, callback) => {
+  let grade;
+
+  switch (true) {
+    case score >= 90:
+      grade = "A";
+      break;
+    case score >= 80:
+      grade = "B";
+      break;
+    case score >= 70:
+      grade = "C";
+      break;
+    case score >= 40:
+      grade = "D";
+      break;
+    case score >= 0:
+      grade = "F";
+      break;
+    default:
+      grade = "Invalid grade";
+      break;
+  }
+  return callback(grade);
+};
+
+let result = getGrade(85, judge);
+console.log(`You got a ${result} result`);
+result = getGrade(95, judge);
+console.log(`You got a ${result} result`);
+result = getGrade(25, judge);
+console.log(`You got a ${result} result`);
