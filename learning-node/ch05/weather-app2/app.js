6940c67ac900e799061fbbd1a35cba4e9640a543
timothy adigun <adigun.timothy@gmail.com>
2026-06-19 10:49:17 +0100

    using fetch and others for js

--- learning-node/ch05/weather-app2/app.js
+++ learning-node/ch05/weather-app2/app.js
@@ -1,4 +1,16 @@
 "use strict";
 
-console.log("Starts Here!");
+import yargs, { demand } from "yargs";
+import { hideBin } from "yargs/helpers";
 
+const args = yargs(hideBin(process.argv))
+  .options({
+    address: {
+      describe: "Address looking for.",
+      demand: true,
+      alias: "a",
+      string: true,
+    },
+  })
+  .help()
+  .alias("help", "h").argv;
