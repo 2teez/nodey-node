36ff546f522338428ffdc211e2cfde70718be76e
timothy adigun <adigun.timothy@gmail.com>
2026-06-19 10:04:31 +0100

    using request and yargs with openweatherapi pull weather info for cities.

--- learning-node/ch05/weather-app/app.js
+++ learning-node/ch05/weather-app/app.js
@@ -2,12 +2,36 @@
 
 const yargs = require("yargs");
 //import { hideBin } from "yargs/helpers"; // for ES MOdule in js
+const request = require("request");
 
 const argv = yargs
-  .command("address", "Address to locate", {
-    describe: "Address to locate",
-    demand: true,
-    alias: "a",
+  .options({
+    a: {
+      demand: true,
+      alias: "address",
+      describe: "Address to fetch weather for",
+      string: true,
+    },
   })
-  .help().argv;
-console.log(JSON.stringify(argv, null, 2));
+  .help()
+  .alias("help", "h").argv;
+//console.log(JSON.stringify(argv, null, 2));
+//
+const API_KEY = "ab45724cc15822eeff6cc675a219acb4";
+//const url = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${API_KEY}&units=metric`;
+let encodedAddress = encodeURIComponent(argv.address);
+request(
+  {
+    url: `https://api.openweathermap.org/data/2.5/weather?q=${encodedAddress}&appid=${API_KEY}&units=metric`,
+    json: true,
+  },
+  (error, response, body) => {
+    if (error) {
+      console.log("Unable to connect:", error);
+    } else {
+      console.log(`City: ${body["name"]} - ${body["sys"]["country"]}`);
+      console.log(`Latitude: ${body["coord"]["lat"]}`);
+      console.log(`Latitude: ${body["coord"]["lon"]}`);
+    }
+  },
+);
