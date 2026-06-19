7bbde6bcf6f302f8d2a60f66d8dfc8900ff4c633
timothy adigun <adigun.timothy@gmail.com>
2026-06-19 22:38:37 +0100

    weather api adjusted

--- learning-node/ch06/weather-app/geocode/geocode.js
+++ learning-node/ch06/weather-app/geocode/geocode.js
@@ -1,9 +1,10 @@
 "use strict";
 
+require("dotenv").config();
 const request = require("request");
 
 const gecodedAddress = (address) => {
-  const API_KEY = "ab45724cc15822eeff6cc675a219acb4";
+  const API_KEY = process.env.OPENWEATHER_KEY;
   let encodedAddress = encodeURIComponent(address);
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${encodedAddress}&appid=${API_KEY}&units=metric`;
 
@@ -16,12 +17,12 @@ const gecodedAddress = (address) => {
       if (error) {
         throw new Error(`HTTP Error: ${error}`);
       }
-      // console.log(JSON.stringify(body, null, 2));
-      const Lat = body["coord"]["lat"];
-      const Lon = body["coord"]["lon"];
-      const name = body["name"];
+      console.log(JSON.stringify(body, null, 2));
+      /*const Lat = body["coord"]["lat"];
+      const Lon = body.coord.lon;
+      const name = body.name;
       console.log(`Name: ${name}`);
-      console.log(`Latitude: ${Lat}\nLongitude: ${Lon}`);
+      console.log(`Latitude: ${Lat}\nLongitude: ${Lon}`);*/
     },
   );
 };
