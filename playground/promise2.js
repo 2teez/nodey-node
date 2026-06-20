976d711de00aa02c577243e85e52e39d18194a38
timothy adigun <adigun.timothy@gmail.com>
2026-06-20 02:11:31 +0100

    practice with promise in nodejs

--- playground/promise2.js
+++ playground/promise2.js
@@ -1,1 +1,10 @@
 "use strict";
+
+// promise variable
+let somePromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("Hey! it works!");
+  }, 2500);
+}).then((message) => {
+  console.log(`Success: ${message}`);
+});
