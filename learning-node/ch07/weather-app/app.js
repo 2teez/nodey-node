e99c0b469a5e80fc619cd5df279c03d424a1f238
timothy adigun <adigun.timothy@gmail.com>
2026-06-20 10:26:07 +0100

    process the cli of the app

--- learning-node/ch07/weather-app/app.js
+++ learning-node/ch07/weather-app/app.js
@@ -1,4 +1,19 @@
 "use strict";
 
-console.log("Starts Here!");
+import "dotenv/config";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
 
+const argv = yargs(hideBin(process.argv))
+  .options({
+    a: {
+      describe: "Address to be searched",
+      demand: true,
+      alias: "address",
+      string: true,
+    },
+  })
+  .help()
+  .alias("help", "h").argv;
+
+console.log(argv.address);
