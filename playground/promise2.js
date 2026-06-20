832c32581f0d46c5dc3a58efa9d0416a85eeb6a7
timothy adigun <adigun.timothy@gmail.com>
2026-06-20 02:13:30 +0100

    using reject in promise in nodejs

--- playground/promise2.js
+++ playground/promise2.js
@@ -3,8 +3,13 @@
 // promise variable
 let somePromise = new Promise((resolve, reject) => {
   setTimeout(() => {
-    resolve("Hey! it works!");
+    //resolve("Hey! it works!");
+    reject("unable to resolve the issues.");
   }, 2500);
-}).then((message) => {
-  console.log(`Success: ${message}`);
-});
+})
+  .then((message) => {
+    console.log(`Success: ${message}`);
+  })
+  .catch((errMessage) => {
+    console.log(`Err: ${errMessage}`);
+  });
