1bc65afb1b3d7f54ac4f16a5fa3ed5ccd1a898c8
timothy adigun <adigun.timothy@gmail.com>
2026-06-20 08:27:03 +0100

    using promise in nodejs

--- playground/promise2.js
+++ playground/promise2.js
@@ -3,7 +3,7 @@
 // promise variable
 let somePromise = new Promise((resolve, reject) => {
   setTimeout(() => {
-    //resolve("Hey! it works!");
+    resolve("Hey! it works!");
     reject("unable to resolve the issues.");
   }, 2500);
 })
@@ -12,4 +12,26 @@ let somePromise = new Promise((resolve, reject) => {
   })
   .catch((errMessage) => {
     console.log(`Err: ${errMessage}`);
+  });
+
+// using promise to add two number
+//
+const addNumber = (a, b) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (typeof a === "number" && typeof b === "number") {
+        resolve(a + b);
+      } else {
+        reject("The values must be numbers");
+      }
+    }, 1500);
+  });
+};
+
+addNumber(5, 7)
+  .then((result) => {
+    console.log(`Result: ${result}`);
+  })
+  .catch((errMsg) => {
+    console.log(errMsg);
   });
